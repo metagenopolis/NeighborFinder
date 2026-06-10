1e6e3381dc4fa448740579a485192ae414bfdef7
masola <mathilde.sola@inrae.fr>
2026-06-10 11:37:26 +0200

    update of documentation for functions including a copyright

--- R/mclr.R
+++ R/mclr.R
@@ -8,6 +8,7 @@
 # For eps and atleast, users do not have to specify any values. Default should be enough.
 #' @param eps epsilon in eq (2) of the paper "Yoon, Gaynanova, Müller (2019), Frontiers in Genetics". positive shifts to all non-zero compositions. Refer to the paper for more details. eps = absolute value of minimum of log ratio counts plus c.
 #' @param atleast default value is 1. Constant c which ensures all nonzero values to be strictly positive. default is 1.
+#' @details This function implements the mclr normalization introduced in Yoon G, Gaynanova I and Müller CL (2019) Microbial Networks in SPRING - Semi-parametric Rank-Based Correlation and Partial Correlation Estimation for Quantitative Microbiome Data. Front. Genet. 10:516. \doi{10.3389/fgene.2019.00516}
 #'
 #' @return \code{mclr} returns a data matrix of the same dimension with input data matrix.
 #' @export
